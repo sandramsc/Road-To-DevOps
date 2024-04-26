270014015679c50f2f06ae65348ea01db415ba09
Sandra Ashipala <sandraashipala@gmail.com>
2024-04-26 20:30:15 +0200

    Learning Go w/ Traversey M. & Coursera

--- Golang/3-functions/exercises/4-named_return_values/code.go
+++ Golang/3-functions/exercises/4-named_return_values/code.go
@@ -1,10 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main("fmt")
-
-
+func yearsUntilEvents(age int) (yearsUntilAdult, yearsUntilDrinking, yearsUntilCarRental int) {
+	yearsUntilAdult = 18 - age
+	if yearsUntilAdult < 0 {
+		yearsUntilAdult = 0
+	}
+	yearsUntilDrinking = 21 - age
+	if yearsUntilDrinking < 0 {
+		yearsUntilDrinking = 0
+	}
+	yearsUntilCarRental = 25 - age
+	if yearsUntilCarRental < 0 {
+		yearsUntilCarRental = 0
+	}
+	return
+}
 
 // don't edit below this line
 
